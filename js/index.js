192617a9dcfb694a409e23337111d6b274a0268b
chat-t <michaelhrothkopf@gmail.com>
2020-07-02 20:29:53 -0400

    login system beta

--- js/index.js
+++ js/index.js
@@ -11,24 +11,49 @@ function login() {
     For the purposes of the front-end only simple clone that I am creating, I have decided to opt for 
     a simple redirect. Below is the code to query the server to prove I know how.
     */
-    /*
+    
     _username = document.getElementById("username");
     _password = document.getElementById("password");
 
     username = _username.value;
     password = _password.value;
+    
+    iscreatingnewaccount = 0;
+    checkbox = document.getElementById("createnewaccount");
+    if (checkbox.checked == true) {
+        iscreatingnewaccount = 1;
+    }
+    
 
     var http = new XMLHttpRequest();
     http.open("POST", 'php/login.php', true);
     http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     http.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            if (this.responseText == "true") {
-                window.location.href = "App.html";
+        if (this.readyState == 4) {
+            /*
+            if (this.status == 200) {
+                if (this.responseText == "true") {
+                    //window.location.href = "App.html";
+                }
+            } else if (this.status == 403) {
+                document.getElementById("errorText").innerHTML = "Error! Incorrect password.";
+                wait(3000);
+                document.getElementById("errorText").innerHTML = "";
+            }
+            */
+           
+            switch(this.status) {
+                case 900:
+                    window.location.href = "App.html";
+                case 901:
+                    document.getElementById("errorText").innerHTML = "Error! Incorrect username or password.";
+                case 905:
+                    document.getElementById("errorText").innerHTML = "Account doesn't exist.";
+                case 912:
+                    document.getElementById("errorText").innerHTML = "Error! Username already taken.";
             }
         }
+        
     }
-    http.send("username="+username+"&password="+password);
-    */
-   window.location.href = "App.html";
+    http.send("username="+username+"&password="+password+"&newaccount="+toString(iscreatingnewaccount));
 }
--- php/login.php
+++ php/login.php
@@ -1,7 +1,47 @@
 <?php
-require_once("../vendors/DataStoreLib/DataStoreLib.php");
+$username = $_POST["username"];
+$password = $_POST["password"];
+$useraccountcreated = ($_POST["password"] == "1") ? true : false;
 
-http_response_code(200);
+function create_account($username, $password) {
+    $_password = password_hash($password);
+    $data = array();
+    $data->username = $username;
+    $data->password = $password;
+    file_put_contents("users/" . $username . ".json", json_encode($data));
+}
 
-echo "true";
+function verify_account($username, $password) {
+    $filecontents = file_get_contents("users/" . $username . ".json");
+    $data = json_decode($filecontents);
+    if (password_verify($data->password, $password)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if ($useraccountcreated) {
+    if (file_exists("users/" . $username . ".json")) {
+        http_response_code(912);
+        echo "912";
+    } else {
+        create_account($username, $password);
+        http_response_code(900);
+        echo "900";
+    }
+} else {
+    if (file_exists("users/" . $username . ".json")) {
+        if (verify_account($username, $password)) {
+            http_response_code(900);
+            echo "900";
+        } else {
+            http_response_code(901);
+            echo "901";
+        }
+    } else {
+        http_response_code(905);
+        echo "905";
+    }
+}
 ?>
